dc890da0fc546380b427e0f83b5eaa1c007c2382
Liz Verde <>
2018-01-05 14:34:02 +0000

    Initialising masonry again for the header/hero section. Experimenting with sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,15 +26,20 @@ $(function() {
 	
 });
 
-// var elem = document.querySelector('.grid');
-// var msnry = new Masonry( elem, {
-//   // options
-//   itemSelector: '.grid-item',
-//   columnWidth: 200
-// });
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
 
-// // element argument can be a selector string
-// //   for an individual element
-// var msnry = new Masonry( '.grid', {
-//   // options
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
+
+// $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   columnWidth: 160
 // });
