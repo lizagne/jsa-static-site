e5bf71e376dba1531fe423af808dd7521aa1d503
Liz Verde <>
2018-01-05 12:41:27 +0000

    Adding all the artist pictures for the homepage, and creating the flexbox rather than masonry

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,15 +26,15 @@ $(function() {
 	
 });
 
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
+// var elem = document.querySelector('.grid');
+// var msnry = new Masonry( elem, {
+//   // options
+//   itemSelector: '.grid-item',
+//   columnWidth: 200
+// });
 
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-  // options
-});
+// // element argument can be a selector string
+// //   for an individual element
+// var msnry = new Masonry( '.grid', {
+//   // options
+// });
